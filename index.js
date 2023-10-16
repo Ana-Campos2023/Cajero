9b52ea0cc30aaa7f08751d9c6bba15d90fd18f04
Ana-hernandez189 <128433087+Ana-hernandez189@users.noreply.github.com>
2023-10-16 11:33:26 -0600

    logos y paginas
    Aqui agrego la pagina redireccionada cuando el usuario accede

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ let contraseñas = ["a12", "l13", "e14", "i15"];
 
 let usuario = document.getElementById("usuario");
 let contra = document.getElementById("password");
-
+let ver;
 inicio.addEventListener('click', () =>{
     for (let i = 0; i < usuarios.length; i++) {
         if (usuario.value === usuarios[i] && contra.value === contraseñas[i]) {
-            console.log('Inicio de sesión exitoso. ¡Bienvenid@, ' + usuario.value + '!');
+            alert('Inicio de sesión exitoso. ¡Bienvenid@, ' + usuario.value + '!');
+            window.location.href = "cuenta.html"
             return;
         }
     }
-    console.log('Error: Usuario o contraseña incorrectos');
+    alert('Error: Usuario o contraseña incorrectos');
 });
+
